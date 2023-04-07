7bad17b0d90c74b037b2a4d23d20110b8f68a11a
Brijendra-Singh2003 <brijendra0369@gmail.com>
2023-04-07 14:54:18 +0530

    added pages headers

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function initiate() {
     currPage = document.getElementById("home");
     currPage.style.borderBottom = '7px solid rgb(26, 255, 0)';
     currPage.style.backgroundImage = 'linear-gradient( rgba(0, 0, 0, 0),rgba(0, 0, 0, 0.196))';
-    home();
+    // home();
 }
 
 function pageChange(h) {
@@ -24,45 +24,118 @@ function home() {
             <div class="bar bar1"></div>
             <div class="bar bar2"></div>
             <div class="bar bar3"></div>
-            <div class="menu">
+            <div class="home_menu menu">
                 <a href="./images/Batman_logo_PNG13.png" target="_blank">Settings</a><hr>
                 <a href="./images/Batman_logo_PNG13.png" target="_blank">About</a><hr>
                 <a href="./images/Batman_logo_PNG13.png" target="_blank">Contact us</a>
             </div>
         </div>
         <div class="search flex">
-            <input type="text" name="search" plceolder="Search" id="search" onclick="tap(this)" class="search_bar">
+            <input type="text" name="search" placeholder="search" id="search" onclick="tap(this)" class="search_bar">
             <button type="submit" class="search_button"></button>
         </div>
         <img src="./images/Batman_logo_PNG13.png" alt="profile" class="logo">
     `;
     main.innerHTML = `
-        <div class="prev"></div>
-        <div class="recomend">
-            <div class="slide slide1"></div>
-            <div class="slide slide2"></div>
-            <div class="slide slide3"></div>
-            <div class="slide slide4"></div>
-            <div class="dots">
-                <div class="dot1"></div>
-                <div class="dot2"></div>
-                <div class="dot3"></div>
-                <div class="dot4"></div>
+        <div class="flex banner" class="main">
+            <div class="prev"></div>
+            <div class="recomend">
+                <div class="slide slide1"></div>
+                <div class="slide slide2"></div>
+                <div class="slide slide3"></div>
+                <div class="slide slide4"></div>
+                <div class="dots">
+                    <div class="dot1"></div>
+                    <div class="dot2"></div>
+                    <div class="dot3"></div>
+                    <div class="dot4"></div>
+                </div>
             </div>
+            <div class="next"></div>
         </div>
-        <div class="next"></div>
     `;
 }
 
 function chats() {
-    head.innerHTML = ``;
-    main.innerHTML = ``;
+    head.innerHTML = `
+    <h1 id="chat_header">Chats</h1>
+    <div class="menu_icon" onclick="myFunction(this)">
+        <div class="bar bar1"></div>
+        <div class="bar bar2"></div>
+        <div class="bar bar3"></div>
+        <div class="chat_menu menu">
+          <a href="./images/Batman_logo_PNG13.png" target="_blank">Settings</a>
+          <hr>
+          <a href="./images/Batman_logo_PNG13.png" target="_blank">About</a>
+          <hr>
+          <a href="./images/Batman_logo_PNG13.png" target="_blank">Contact us</a>
+        </div>
+    </div>
+    `;
+    main.innerHTML = `
+    <div class="chat_list">
+        <div class="chat">
+            <div class="dp flex"><img src="#" alt="dp"></div>
+            <div class="name flex">name</div>
+            <div class="last_m">last massage</div>
+        </div>
+        
+        <div class="chat">
+            <div class="dp flex"><img src="#" alt="dp"></div>
+            <div class="name flex">name</div>
+            <div class="last_m">last massage</div>
+        </div>
+        
+        <div class="chat">
+            <div class="dp flex"><img src="#" alt="dp"></div>
+            <div class="name flex">name</div>
+            <div class="last_m">last massage</div>
+        </div>
+        
+        <div class="chat">
+            <div class="dp flex"><img src="#" alt="dp"></div>
+            <div class="name flex">name</div>
+            <div class="last_m">last massage</div>
+        </div>
+        
+    </div>
+
+    <div class="new_chat"></div>
+    `;
 }
 function feed() {
-    head.innerHTML = ``;
+    head.innerHTML = `
+    <h1 id="chat_header">Feed</h1>
+    <div class="menu_icon" onclick="myFunction(this)">
+        <div class="bar bar1"></div>
+        <div class="bar bar2"></div>
+        <div class="bar bar3"></div>
+        <div class="chat_menu menu">
+          <a href="./images/Batman_logo_PNG13.png" target="_blank">Settings</a>
+          <hr>
+          <a href="./images/Batman_logo_PNG13.png" target="_blank">About</a>
+          <hr>
+          <a href="./images/Batman_logo_PNG13.png" target="_blank">Contact us</a>
+        </div>
+    </div>
+    `;
     main.innerHTML = ``;
 }
 function notification() {
-    head.innerHTML = ``;
+    head.innerHTML = `
+    <h1 id="chat_header">Notifications</h1>
+    <div class="menu_icon" onclick="myFunction(this)">
+        <div class="bar bar1"></div>
+        <div class="bar bar2"></div>
+        <div class="bar bar3"></div>
+        <div class="chat_menu menu">
+          <a href="./images/Batman_logo_PNG13.png" target="_blank">Settings</a>
+          <hr>
+          <a href="./images/Batman_logo_PNG13.png" target="_blank">About</a>
+          <hr>
+          <a href="./images/Batman_logo_PNG13.png" target="_blank">Contact us</a>
+        </div>
+    </div>
+    `;
     main.innerHTML = ``;
 }
